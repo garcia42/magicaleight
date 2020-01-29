071f2bedacef974fefea08f437564063d113fd6c
Jesus <jesusvgarcia42@berkeley.edu>
2020-01-28 19:53:42 -0800

    Fix the path

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get('.well-known/acme-challenge/2pJchLtsoZ_k03aoi5x4n4QLWSrWsJNWJ5m5487Mx7Y', function(req, res) {
+app.get('/.well-known/acme-challenge/2pJchLtsoZ_k03aoi5x4n4QLWSrWsJNWJ5m5487Mx7Y', function(req, res) {
   res.send('2pJchLtsoZ_k03aoi5x4n4QLWSrWsJNWJ5m5487Mx7Y.Erld3WnwKNjh6eYpUCNzY4ivDXQFMr4iT1ghUciAzVw')
 })
 
